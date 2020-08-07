59277570f7ba0dca21e27d3d8cdba4a568b0bc02
Dimzt <dimas12345yuio@gmail.com>
2020-08-07 11:00:24 +0700

    added random color button in color.js

--- color.js
+++ color.js
@@ -1,4 +1,22 @@
-const tombol = document.getElementById('tUbahWarna');
-tombol.addEventListener('click', function () {
+const button = document.getElementById('tUbahWarna');
+button.addEventListener('click', function () {
   document.body.classList.toggle('merah-muda');
+});
+
+// create button
+const buttonNew = document.createElement('button');
+const textButton = document.createTextNode('Ubah Warna');
+buttonNew.append(textButton);
+// add attribut
+buttonNew.setAttribute('type', 'button');
+
+button.after(buttonNew);
+
+// events
+buttonNew.addEventListener('click', function () {
+  const r = Math.floor(Math.random() * 255 + 1);
+  const g = Math.floor(Math.random() * 255 + 1);
+  const b = Math.floor(Math.random() * 255 + 1);
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 })
